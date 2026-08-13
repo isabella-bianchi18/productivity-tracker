db2f8221e3f2ae47a766823c71dae83e03ba3a7e
isabella-bianchi18 <80981172+isabella-bianchi18@users.noreply.github.com>
2026-08-13 15:33:29 -0400

    4.4.9

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service worker with cache busting for PWA updates
-const CACHE_VERSION = 'v4.4.8';
+const CACHE_VERSION = 'v4.4.9';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
