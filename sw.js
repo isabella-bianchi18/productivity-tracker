7cee43f470c758676372a129d5c41ee733cb5232
isabella-bianchi18 <80981172+isabella-bianchi18@users.noreply.github.com>
2026-08-18 09:05:39 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service worker with cache busting for PWA updates
-const CACHE_VERSION = 'v5.8.13';
+const CACHE_VERSION = 'v5.8.15';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
